6e622f208590f1502d8e8b6a2bd30fa0322245dc
prudhvi <prudhvi@M1-Air.local>
2024-04-30 15:52:27 +0530

    Added orcle library for stale prices

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -9,6 +9,7 @@ import {Test} from "forge-std/Test.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralisedStableCoin} from "../../src/DecentralisedStableCoin.sol";
 import {ERC20Mock} from "@openzeppelin/contracts/mocks/ERC20Mock.sol";
+import {MockV3Aggregator} from "../mocks/MockV3Aggregator.sol";
 
 contract Handler is Test {
     DSCEngine engine;
@@ -18,8 +19,8 @@ contract Handler is Test {
     ERC20Mock wbtc;
 
     uint256 public timesMintIsCalled;
-
     address[] public userWithCollateralDeposited;
+    MockV3Aggregator public ethUsdPriceFeed;
 
     uint256 MAX_DEPOSIT_SIZE = type(uint96).max;
 
@@ -30,6 +31,8 @@ contract Handler is Test {
         address[] memory collateralTokens = engine.getCollateralTokens();
         wbtc = ERC20Mock(collateralTokens[0]);
         weth = ERC20Mock(collateralTokens[1]);
+
+        ethUsdPriceFeed = MockV3Aggregator(engine.getCollateralTokenPriceFeed(address(weth)));
     }
 
     function mintDsc(uint256 amount, uint256 addressSeed) public {
@@ -70,16 +73,6 @@ contract Handler is Test {
 
     // redeem collateral <-
 
-    // function redeemCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
-    //     ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
-    //     uint256 maxCollateralToRedeem = engine.getCollateralBalanceOfUser(msg.sender, address(collateral));
-    //     amountCollateral = bound(amountCollateral, 0, maxCollateralToRedeem);
-    //     if (amountCollateral == 0) {
-    //         return;
-    //     }
-    //     vm.prank(msg.sender);
-    //     engine.redeemCollateral(address(collateral), amountCollateral);
-    // }
     function redeemCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
         ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
         uint256 maxCollateralToRedeem = engine.getCollateralBalanceOfUser(msg.sender, address(collateral));
@@ -90,6 +83,12 @@ contract Handler is Test {
         vm.prank(msg.sender);
         engine.redeemCollateral(address(collateral), amountCollateral);
     }
+
+    // This breaks our invariant test suite!!!
+    // function updateCollateralPrice(uint96 newPrice) public {
+    //     int256 newPriceInt = int256(uint256(newPrice));
+    //     ethUsdPriceFeed.updateAnswer(newPriceInt);
+    // }
 
     // Helper Functions
     function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock) {
